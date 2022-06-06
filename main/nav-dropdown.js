8911ee1cdc2d00cbd8b1c12985d250d811802dcd
LEELA-SRI <leelasric@gmail.com>
2022-06-07 02:03:12 +0530

    update and possibly make it neat

--- main/nav-dropdown.js
+++ main/nav-dropdown.js
@@ -1,37 +1,36 @@
-'use strict'
-const nav_mainPage = document.getElementsByClassName('nav-mainPage');
-const nav_catPage = document.getElementsByClassName('nav-catalogPage');
-const nav_commPage = document.getElementsByClassName('nav-commPage');
-const nav_resPage = document.getElementsByClassName('nav-resPage');
-const nav_priPage = document.getElementsByClassName('nav-priPage');
+"use strict";
+const nav_mainPage = document.getElementsByClassName("nav-mainPage");
+const nav_catPage = document.getElementsByClassName("nav-catalogPage");
+const nav_commPage = document.getElementsByClassName("nav-commPage");
+const nav_resPage = document.getElementsByClassName("nav-resPage");
+const nav_priPage = document.getElementsByClassName("nav-priPage");
 
 var scrollbar = window.scrollY;
 
 if (scrollbar > 20) {
-    document.getElementsByClassName('mob-nav')[0].style.background = "#FFF";
-    document.getElementsByClassName('mob-nav')[0].style.color = "#1A191D";
+    document.getElementsByClassName("mob-nav")[0].style.background = "#FFF";
+    document.getElementsByClassName("mob-nav")[0].style.color = "#1A191D";
 
-    document.getElementsByClassName('mob-nav-frame-right')[0].style.filer = "invert(100%)";
+    document.getElementsByClassName("mob-nav-frame-right")[0].style.filer =
+        "invert(100%)";
 
-    document.getElementsByClassName('nav-bar-desktop')[0].style.background = "#FFF";
-    document.getElementsByClassName('nav-bar-desktop')[0].style.color = "#121212";
+    document.getElementsByClassName("nav-bar-desktop")[0].style.background =
+        "#FFF";
+    document.getElementsByClassName("nav-bar-desktop")[0].style.color = "#121212";
+} else {
+    document.getElementsByClassName("nav-bar-desktop")[0].style.background =
+        "#1A191D";
+    document.getElementsByClassName("nav-bar-desktop")[0].style.color = "#FFF";
 
+    document.getElementsByClassName("mob-nav")[0].style.background = "#1A191D";
+    document.getElementsByClassName("mob-nav")[0].style.color = "#fff";
 
-}
-else {
-    document.getElementsByClassName('nav-bar-desktop')[0].style.background = "#1A191D";
-    document.getElementsByClassName('nav-bar-desktop')[0].style.color = "#FFF";
-
-    document.getElementsByClassName('mob-nav')[0].style.background = "#1A191D";
-    document.getElementsByClassName('mob-nav')[0].style.color = "#fff";
-
-    document.getElementsByClassName('mob-nav-frame-right')[0].style.filter = "invert(0%)";
-
-
+    document.getElementsByClassName("mob-nav-frame-right")[0].style.filter =
+        "invert(0%)";
 }
 
 function hideMobNavigationMenu() {
-    document.querySelector('body').style.overflowY="scroll"
+    document.querySelector("body").style.overflowY = "scroll";
     nav_commPage[0].style.display = "none";
     nav_catPage[0].style.display = "none";
     nav_mainPage[0].style.display = "none";
@@ -40,7 +39,7 @@ function hideMobNavigationMenu() {
 }
 
 function showMobMainNavigationMenu() {
-    document.querySelector('body').style.overflow="hidden"
+    document.querySelector("body").style.overflow = "hidden";
     nav_commPage[0].style.display = "none";
     nav_catPage[0].style.display = "none";
     nav_mainPage[0].style.display = "block";
@@ -80,29 +79,30 @@ function showMobPriNavigationMenu() {
     nav_priPage[0].style.display = "block";
 }
 
-window.addEventListener('scroll', function (event) {
+window.addEventListener("scroll", function (event) {
     var scrollbar = window.scrollY;
     if (scrollbar > 20) {
-        document.getElementsByClassName('mob-nav')[0].style.background = "#FFF";
-       
-        document.getElementsByClassName('mob-nav')[0].style.color = "#1A191D";
+        document.getElementsByClassName("mob-nav")[0].style.background = "#FFF";
 
-        document.getElementsByClassName('mob-nav-frame-right')[0].style.filter = "invert(100%)";
+        document.getElementsByClassName("mob-nav")[0].style.color = "#1A191D";
 
-        document.getElementsByClassName('nav-bar-desktop')[0].style.background = "#FFF";
-        document.getElementsByClassName('nav-bar-desktop')[0].style.color = "#121212";
-    }
-    else {
-        document.getElementsByClassName('nav-bar-desktop')[0].style.background = "#1A191D";
-        document.getElementsByClassName('nav-bar-desktop')[0].style.color = "#FFF";
+        document.getElementsByClassName("mob-nav-frame-right")[0].style.filter =
+            "invert(100%)";
 
-        document.getElementsByClassName('mob-nav-frame-right')[0].style.filter = "invert(0%)";
+        document.getElementsByClassName("nav-bar-desktop")[0].style.background =
+            "#FFF";
+        document.getElementsByClassName("nav-bar-desktop")[0].style.color =
+            "#121212";
+    } else {
+        document.getElementsByClassName("nav-bar-desktop")[0].style.background =
+            "#1A191D";
+        document.getElementsByClassName("nav-bar-desktop")[0].style.color = "#FFF";
 
-        document.getElementsByClassName('mob-nav')[0].style.background = "#1A191D";
-        
-        document.getElementsByClassName('mob-nav')[0].style.color = "#fff";
+        document.getElementsByClassName("mob-nav-frame-right")[0].style.filter =
+            "invert(0%)";
+
+        document.getElementsByClassName("mob-nav")[0].style.background = "#1A191D";
+
+        document.getElementsByClassName("mob-nav")[0].style.color = "#fff";
     }
 });
-
-
-
